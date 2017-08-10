c49401a3983f1860993a0ce162edcf76d6a61366
Chuck Rolke <crolke@redhat.com>
2017-08-10 11:35:12 -0400

    getting links to send partial messages at the same time

--- examples/c/proactor/send.c
+++ examples/c/proactor/send.c
@@ -37,7 +37,7 @@
 #include <string.h>
 
 #define MESSAGE_SIZE (2048 * 1024)
-#define SEND_BATCH_SIZE 2
+#define LINK_CHUNK_SIZE (16 * 1024)
 
 #define fatal(...) do {                                 \
     fprintf(stderr, "%s:%d: ", __FILE__, __LINE__);     \
@@ -70,6 +70,11 @@ typedef struct app_data_t {
 
   app_instance_t l1;
   app_instance_t l2;
+
+  bool       message_in_progress;
+  size_t     bytes_sent;
+  pn_bytes_t msgbuf;
+  
 } app_data_t;
 
 static int exit_code = 0;
@@ -95,7 +100,7 @@ static pn_bytes_t encode_message(app_data_t* app, app_instance_t* inst) {
   pn_data_enter(body);
 
   //pn_data_put_string(body, pn_bytes(sizeof("sequence")-1, "sequence"));
-  static const size_t msg_size = MESSAGE_SIZE;
+  size_t msg_size = app->l1.message_size;
   char * mbufptr = (char *)malloc(msg_size);
   for (size_t i=0; i<msg_size; i++)
       mbufptr[i] = '.';
@@ -130,6 +135,29 @@ static pn_bytes_t encode_message(app_data_t* app, app_instance_t* inst) {
   return pn_bytes(mbuf.size, mbuf.start);
 }
 
+
+/* Sends a chunk of data to each link */
+static void send_chunk(app_data_t* app,
+                       pn_link_t *sender,
+                       pn_link_t *sender2,
+                       app_instance_t *inst,
+                       app_instance_t *inst2) {
+    size_t bytesremaining = app->msgbuf.size - app->bytes_sent;
+    size_t bytes2send = LINK_CHUNK_SIZE < bytesremaining ? LINK_CHUNK_SIZE : bytesremaining;
+    pn_link_send(sender,  app->msgbuf.start + app->bytes_sent, bytes2send);
+    pn_link_send(sender2, app->msgbuf.start + app->bytes_sent, bytes2send);
+    app->bytes_sent += bytes2send;
+    PRINTF(", BOTH LINKS: sent block of %zu bytes, total sent: %zu\n", bytes2send, app->bytes_sent);
+    bytesremaining = app->msgbuf.size - app->bytes_sent;
+    if (bytesremaining == 0) {
+        pn_link_advance(sender);
+        pn_link_advance(sender2);
+        inst->sent += 1;
+        inst2->sent += 1; //2
+        app->message_in_progress = false;
+    }
+}
+
 /* Returns true to continue, false if finished */
 static bool handle(app_data_t* app, pn_event_t* event) {
   log_this(event, "ENTER");
@@ -152,45 +180,28 @@ static bool handle(app_data_t* app, pn_event_t* event) {
 
    case PN_LINK_FLOW: {
      /* The peer has given us some credit, now we can send messages */
-     pn_link_t *sender = pn_event_link(event);
-     app_instance_t *inst;
-     app_instance_t *inst2; //2
-     bool is_l1 = sender == app->l1.link;
-     inst  = is_l1 ? &app->l1 : &app->l2;
-     inst2 = is_l1 ? &app->l2 : &app->l1; //2
-     pn_link_t *sender2 = is_l1 ? app->l2.link : app->l1.link; //2
-     int sent = 0;
-     while (
-         (pn_link_credit(sender) > 0) && 
-         (inst->sent < inst->message_count) && 
-         (sent < SEND_BATCH_SIZE)) {
-       PRINTF(", link %s, credit:%d = pn_link_credit()\n", (is_l1 ? "l1" : "l2"), pn_link_credit(sender));
-       inst->sent += 1;
-       inst2->sent += 1; //2
+     pn_link_t      *sender = pn_event_link(event);
+     bool is_l1             = sender == app->l1.link;
+
+     pn_link_t      *sender2 = is_l1 ? app->l2.link : app->l1.link; //2
+     app_instance_t *inst    = is_l1 ? &app->l1 : &app->l2;
+     app_instance_t *inst2   = is_l1 ? &app->l2 : &app->l1; //2
+
+     // deal with message in progress
+     if (app->message_in_progress) {
+        send_chunk(app, sender, sender2, inst, inst2);
+        break;
+     }
+
+     // start new message, maybe
+     if  (pn_link_credit(sender) > 0 && 
+         (inst->sent < inst->message_count)) {
+       PRINTF(", Start message on links.  %s, credit:%d = pn_link_credit()\n", (is_l1 ? "l1" : "l2"), pn_link_credit(sender));
        // Use sent counter as unique delivery tag.
        pn_delivery(sender, pn_dtag((const char *)&inst->sent, sizeof(inst->sent)));
        pn_delivery(sender2, pn_dtag((const char *)&inst2->sent, sizeof(inst2->sent))); //2
-       pn_bytes_t msgbuf = encode_message(app, inst);
-       {
-         size_t bytessent = 0;
-         static const size_t linksend_size = 100;
-         while (bytessent < msgbuf.size) {
-             size_t bytesremaining = msgbuf.size - bytessent;
-             size_t bytes2send = linksend_size < bytesremaining ? linksend_size : bytesremaining;
-             pn_link_send(sender,  msgbuf.start + bytessent, bytes2send);
-             pn_link_send(sender2, msgbuf.start + bytessent, bytes2send);
-             bytessent += bytes2send;
-             PRINTF(", BOTH LINKS: bucked %zu bytes, total sent: %zu\n", bytes2send, bytessent);
-         }
-       }
-       pn_link_advance(sender);
-       pn_link_advance(sender2);
-
-       PRINTF(", pn_delivery(sender; pn_dtag((const char *)&app->sent; sizeof(app->sent)));\n");
-       PRINTF(", pn_bytes_t msgbuf = encode_message(app);\n");
-       PRINTF(", pn_link_send(sender; msgbuf.start; msgbuf.size:%zd);\n", msgbuf.size);
-       PRINTF(", pn_link_advance(sender);\n");
-        sent++;
+       app->msgbuf = encode_message(app, inst);
+       send_chunk(app, sender, sender2, inst, inst2);
      }
      break;
    }
