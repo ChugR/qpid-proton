b04f441407ee2fb0de346af56dc484c9f7fe28c0
Chuck Rolke <crolke@redhat.com>
2017-09-22 15:47:05 -0400

    clean up for commit

--- examples/c/send-abort.c
+++ examples/c/send-abort.c
@@ -32,25 +32,18 @@
 #include <stdlib.h>
 
 /*
- * This hack sends aborted messages.
- * Each messages process:
- *  Generate a tag and a message data buffer
- *  Send half the data
- *  Wait for a Flow signalling that outbound_bytes has changed
- *  Abort the delivery
- * Command line arg 5 may be '0' to do original send
- *  to see if *that* still works.
- * Data is a biggish string that will exceed a frame size and
- *  hopefully get proton to push it so an abort is not on the first
- *  frame of a message.
- * Data gets sent in two chunks:
- *  From beginning, len=total-HOLDBACK
- *  From beginning+HOLDBACK, len=HOLDBACK
+ * Send aborted messages.
+ * 
+ * An 80 Kbyte (MSG_SIZE) message is generated.
+ * The size is chosen to be large enough so that some of the message
+ * will go out on the wire before the abort is sent.
+ * 
+ * 79 Kbytes (MSG_SIZE - HOLDBACK) are sent on one link flow event.
+ * The message is aborted on the next link flow event.
  */
 
-#define STRING_MESSAGE_SIZE 80000
-
-#define HOLDBACK 1000
+#define MSG_SIZE 80000
+#define HOLDBACK  1000
 
 typedef struct app_data_t {
   const char *host, *port;
@@ -61,8 +54,7 @@ typedef struct app_data_t {
   pn_proactor_t *proactor;
   pn_rwbytes_t message_buffer;
   int sent;
-  int acknowledged;
-  int strategy;     // 0->do original thing, else->abort all messages
+  int aborted;
   bool in_progress;
   pn_bytes_t msgbuf;
 } app_data_t;
@@ -82,17 +74,18 @@ static void check_condition(pn_event_t *e, pn_condition_t *cond) {
 static pn_bytes_t encode_message(app_data_t* app) {
   /* Construct a message with the map { "sequence": app.sent } */
   pn_message_t* message = pn_message();
-  char ddd[STRING_MESSAGE_SIZE];
-  for (size_t i=0; i<sizeof(ddd); i++) ddd[i] = '?';
+  char data[MSG_SIZE + 11];
+  for (size_t i=0; i<MSG_SIZE; i+=10) 
+      sprintf(&data[i], "<%09zu", i);
   pn_data_put_int(pn_message_id(message), app->sent); /* Set the message_id also */
   pn_data_t* body = pn_message_body(message);
   pn_data_enter(body);
-  pn_data_put_string(body, pn_bytes(sizeof(ddd), ddd));
+  pn_data_put_string(body, pn_bytes(MSG_SIZE, data));
   pn_data_exit(body);
 
   /* encode the message, expanding the encode buffer as needed */
   if (app->message_buffer.start == NULL) {
-    static const size_t initial_size = STRING_MESSAGE_SIZE + 1000;
+    static const size_t initial_size = MSG_SIZE + 1000;
     app->message_buffer = pn_rwbytes(initial_size, (char*)malloc(initial_size));
   }
   /* app->message_buffer is the total buffer space available. */
@@ -131,35 +124,22 @@ static bool handle(app_data_t* app, pn_event_t* event) {
    case PN_LINK_FLOW: {
      /* The peer has given us some credit, now we can send messages */
      pn_link_t *sender = pn_event_link(event);
-     printf("Link Flow event: pn_link_credit(sender)=%d, sent=%d, msg_count=%d\n", pn_link_credit(sender), app->sent, app->message_count);
     while (app->in_progress || (pn_link_credit(sender) > 0 && app->sent < app->message_count)) {
         if (!app->in_progress) {
             // Use sent counter as unique delivery tag.
             pn_delivery(sender, pn_dtag((const char *)&app->sent, sizeof(app->sent)));
-            pn_delivery_t * pnd = pn_link_current(sender);
-            printf("Link flow first part. delivery at %p\n", (void*)pnd);
             app->msgbuf = encode_message(app);
             pn_link_send(sender, app->msgbuf.start, app->msgbuf.size - HOLDBACK); // Don't send it all
             app->in_progress = true;
             break;
         } else {
             pn_delivery_t * pnd = pn_link_current(sender);
-            printf("Link flow second part. delivery at %p\n", (void*)pnd);
-            if (app->strategy == 0) {
-                // This sends the message like normal
-                pn_link_send(sender, app->msgbuf.start + HOLDBACK, HOLDBACK);
-                pn_link_advance(sender);
-            } else {
-                // Here we get devious
-                pn_delivery_abort(pnd);
-                // aborted delivery are presettled and never ack'd. So pretend that happened here.
-                if (++app->acknowledged == app->message_count) {
-                    printf("%d messages sent and aborted\n", app->acknowledged);
-                    // Testing: don't close connection. Let everything dribble out onto the wire
-                    //pn_connection_close(pn_event_connection(event));
-
-                    /* Continue handling events till we receive TRANSPORT_CLOSED */
-                }
+            pn_delivery_abort(pnd);
+            // aborted delivery is presettled and never ack'd.
+            if (++app->aborted == app->message_count) {
+                printf("%d messages sent and aborted\n", app->aborted);
+                pn_connection_close(pn_event_connection(event));
+                /* Continue handling events till we receive TRANSPORT_CLOSED */
             }
             ++app->sent;
             app->in_progress = false;
@@ -171,17 +151,9 @@ static bool handle(app_data_t* app, pn_event_t* event) {
    case PN_DELIVERY: {
      /* We received acknowledgedment from the peer that a message was delivered. */
      pn_delivery_t* d = pn_event_delivery(event);
-     if (pn_delivery_remote_state(d) == PN_ACCEPTED) {
-       if (++app->acknowledged == app->message_count) {
-         printf("%d messages sent and acknowledged\n", app->acknowledged);
-         pn_connection_close(pn_event_connection(event));
-         /* Continue handling events till we receive TRANSPORT_CLOSED */
-       }
-     } else {
-       fprintf(stderr, "unexpected delivery state %d\n", (int)pn_delivery_remote_state(d));
-       pn_connection_close(pn_event_connection(event));
-       exit_code=1;
-     }
+     fprintf(stderr, "Aborted deliveries should not receive delivery events. Delivery state %d\n", (int)pn_delivery_remote_state(d));
+     pn_connection_close(pn_event_connection(event));
+     exit_code=1;
      break;
    }
 
@@ -231,9 +203,8 @@ int main(int argc, char **argv) {
   app.container_id = argv[0];   /* Should be unique */
   app.host = (argc > 1) ? argv[1] : "";
   app.port = (argc > 2) ? argv[2] : "amqp";
-  app.amqp_address = (argc > 3) ? argv[3] : "jms.queue.qpid-interop.abort-test";
+  app.amqp_address = (argc > 3) ? argv[3] : "examples";
   app.message_count = (argc > 4) ? atoi(argv[4]) : 10;
-  app.strategy = (argc > 5) ? atoi(argv[5]) : 1; // default to abort, specify 0 to do original
 
   app.proactor = pn_proactor();
   char addr[PN_MAX_ADDR];
